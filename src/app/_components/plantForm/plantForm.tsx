c550af4d740f2ecdfc42792de57fac094ac8a0de
magdalena.matusiak <magdalena.matusiak@adrino.pl>
2025-02-10 13:14:48 +0100

    use enums for location and visible state

--- src/app/_components/plantForm/plantForm.tsx
+++ src/app/_components/plantForm/plantForm.tsx
@@ -2,33 +2,45 @@
 
 import {useForm, SubmitHandler} from "react-hook-form";
 import PlantFormField from "@/app/_components/plantForm/plantFormField";
+import {LOCATION, VISIBLE_STATE} from "@/app/constants";
 
 type FormData = {
 	name: string;
-	location: string;
+	location: LOCATION | undefined;
 	description: string;
-	visibleState: string;
+	visibleState: VISIBLE_STATE | undefined;
 }
 
 type FormField = {
 	label: string,
 	fieldName: string,
 	value: string,
+	options?: string[];
 };
 
 const PlantForm = () => {
-	const {register, handleSubmit, watch, formState: {errors}} = useForm<FormData>();
+	const {register, handleSubmit, watch, formState: {errors}} = useForm<FormData>({
+		defaultValues: {
+			location: undefined,
+			visibleState: undefined
+		}
+	});
+
 	const onSubmit: SubmitHandler<FormData> = (data) => {
 		localStorage.setItem("plantFormData", JSON.stringify(data));
 		console.log(data);
 	}
 
-
 	const formFields: FormField[] = [
 		{label: "Name", fieldName: "name", value: watch("name")},
-		{label: "Location", fieldName: "location", value: watch("location")},
+		{label: "Location", fieldName: "location", value: watch("location"), options: Object.values(LOCATION)},
 		{label: "Description", fieldName: "description", value: watch("description")},
-		{label: "Visible state", fieldName: "visibleState", value: watch("visibleState")},
+		{
+			label: "Visible state",
+			fieldName: "visibleState",
+			value: watch("visibleState"),
+			options: Object.values(VISIBLE_STATE)
+		},
 	]
 
 	return (
@@ -36,9 +48,9 @@ const PlantForm = () => {
 			<form onSubmit={handleSubmit(onSubmit)} className="p-6 bg-white rounded-lg shadow-md w-full max-w-md">
 				<h2 className="text-xl font-bold mb-4 text-center">Plant Form</h2>
 
-				{formFields.map(({label, fieldName, value}) => (
+				{formFields.map(({label, fieldName, value, options}) => (
 					<PlantFormField key={fieldName} label={label} fieldName={fieldName} register={register}
-					                errors={errors} value={value}/>))}
+					                errors={errors} value={value} options={options}/>))}
 
 				<button type="submit" className="w-full bg-blue-500 text-white p-2 rounded hover:bg-blue-600">Submit
 				</button>
--- src/app/_components/plantForm/plantFormField.tsx
+++ src/app/_components/plantForm/plantFormField.tsx
@@ -6,15 +6,30 @@ interface PlantFormFieldProps {
 	register: ReturnType<typeof useForm>['register'];
 	errors: any;
 	value: string;
+	options?: string;
 }
 
-const PlantFormField = ({label, fieldName, register, errors, value}: PlantFormFieldProps) => {
+const PlantFormField = ({label, fieldName, register, errors, value, options}: PlantFormFieldProps) => {
+	const defaultOption = "---";
+
 	return (
 		<div className="mb-4">
 			<label className="block text-gray-700">{label}</label>
+
 			<div className="flex items-center">
-				<input {...register(`${fieldName}`, {required: "This field is required"})}
-				       className="w-full p-2 border border-gray-300 rounded mt-1"/>
+				{options ? (
+					<select {...register(fieldName, {required: "This field is required"})}
+					        className="w-full p-2 border border-gray-300 rounded mt-1">
+						<option value="">{defaultOption}</option>
+						{options.map((option, index) => (
+							<option key={index} value={option}>
+								{option}
+							</option>
+						))}
+					</select>
+				) : (
+					<input {...register(`${fieldName}`, {required: "This field is required"})}
+					       className="w-full p-2 border border-gray-300 rounded mt-1"/>)}
 				{value && <span className="ml-2">✅</span>}
 			</div>
 			{errors[fieldName] && <span className="text-red-500 text-sm">{errors[fieldName].message}</span>}
