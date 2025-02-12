564e648ef4a13f44bb74d372e13bdb8efbfbe7c5
magdalena.matusiak <magdalena.matusiak@adrino.pl>
2025-02-12 12:50:09 +0100

    fix updating plant, handle watering

--- src/app/utils/localStorageService.ts
+++ src/app/utils/localStorageService.ts
@@ -6,12 +6,30 @@ export const savePlant = (newPlant: PlantData) => {
 	try {
 		const plantToSave = {...newPlant, id: generateUniqueId()};
 
+		//todo temporary for testing watering feature purpose
+
+		// const tenDaysAgo = new Date().setDate(new Date().getDate() - 10);
+		// const plantToSave = {...newPlant, id: generateUniqueId(), lastWatered: tenDaysAgo};
+
 		const existingPlants = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEYS.PLANTS_DATA) || "[]");
 		existingPlants.push(plantToSave);
 
 		localStorage.setItem(LOCAL_STORAGE_KEYS.PLANTS_DATA, JSON.stringify(existingPlants));
 	} catch (error) {
 		console.error("Error saving to local storage", error)
+	}
+}
+
+export const updatePlant = (plantId: string, updatedPlant: PlantData) => {
+	try {
+		const existingPlants = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEYS.PLANTS_DATA) || "[]");
+		const updatedPlants = existingPlants.map((plant: PlantData) => {
+			return plant.id === plantId ? {...updatedPlant} : plant;
+		});
+
+		localStorage.setItem(LOCAL_STORAGE_KEYS.PLANTS_DATA, JSON.stringify(updatedPlants));
+	} catch (error) {
+		console.error("Error updating plant in local storage", error);
 	}
 }
 
@@ -25,7 +43,7 @@ export const getPlants = (): PlantData[] => {
 	}
 }
 
-export const getPlantById = (id: string): PlantData | null => {
+export const getPlantById = (plantId: string): PlantData | null => {
 	try {
 		const data = localStorage.getItem(LOCAL_STORAGE_KEYS.PLANTS_DATA);
 
@@ -34,7 +52,7 @@ export const getPlantById = (id: string): PlantData | null => {
 		}
 
 		const plants: PlantData[] = JSON.parse(data);
-		return plants.find(plant => plant.id === id) || null;
+		return plants.find(plant => plant.id === plantId) || null;
 	} catch (error) {
 		console.error("Error getting data from local storage", error);
 		return null;
@@ -50,5 +68,18 @@ export const removePlant = (plantId: string) => {
 	} catch (error) {
 		console.error("Error removing plant from local storage", error);
 		return [];
+	}
+}
+
+export const waterPlant = (plantId: string) => {
+	try {
+		const existingPlants = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEYS.PLANTS_DATA) || "[]");
+		const updatedPlants = existingPlants.map((plant: PlantData) => {
+			return plant.id === plantId ? {...plant, lastWatered: new Date().toISOString()} : plant;
+		});
+
+		localStorage.setItem(LOCAL_STORAGE_KEYS.PLANTS_DATA, JSON.stringify(updatedPlants));
+	} catch (error) {
+		console.error("Error watering plant in local storage", error);
 	}
 }
