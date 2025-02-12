bda4ac472f3b177661292084f953cc251cfcfe82
magdalena.matusiak <magdalena.matusiak@adrino.pl>
2025-02-12 19:13:52 +0100

    fix eslint problems

--- src/app/_components/main/main.tsx
+++ src/app/_components/main/main.tsx
@@ -6,7 +6,7 @@ import PlantsList from "@/app/_components/plantsList/plantsList";
 import {getPlants} from "@/app/_utils/localStorageService";
 
 const Main = () => {
-	const [data, setData] = useState<PlantData[]>(null);
+	const [data, setData] = useState<PlantData[]>([]);
 
 	const refreshData = () => {
 		const plantsData = getPlants();
@@ -18,7 +18,7 @@ const Main = () => {
 	}, [])
 
 	return (<>
-		{data && (
+		{data.length > 0 && (
 			<PlantsList data={data} refreshData={refreshData}/>
 		)}
 	</>)
